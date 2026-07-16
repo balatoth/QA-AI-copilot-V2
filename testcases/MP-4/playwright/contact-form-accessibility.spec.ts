dc14f6952e3c999d4401ae5610821b40df808933
balatoth <balazs.toth@yahoo.com>
2026-07-16 13:25:19 +0200

    Update testcases/MP-4/playwright/contact-form-accessibility.spec.ts

--- testcases/MP-4/playwright/contact-form-accessibility.spec.ts
+++ testcases/MP-4/playwright/contact-form-accessibility.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 
+const baseUrl = 'https://testsmith-io.github.io/practice-software-testing/#/';
+
 test('TC-001 - Verify Accessibility of Contact Form', async ({ page }) => {
-  await page.goto('https://testsmith-io.github.io/practice-software-testing/#/');
-  const contactLink = await page.getByText('Contact'); // Assuming there's a link to the contact page
+  await page.goto(baseUrl);
+  const contactLink = page.getByRole('link', { name: 'Contact' }); // Assuming there's a link to contact page
   await contactLink.click();
-  const contactForm = await page.getByRole('form');
-  expect(contactForm).toBeVisible();
+  const contactForm = await page.getByRole('form', { name: 'Contact Form' }); // Assuming the form has an accessible name
+  await expect(contactForm).toBeVisible();
 });
