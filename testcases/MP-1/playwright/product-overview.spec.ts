0bb166523ac01904f44cb5b6edda857e7b69c638
balatoth <balazs.toth@yahoo.com>
2026-07-16 13:30:27 +0200

    Update testcases/MP-1/playwright/product-overview.spec.ts

--- testcases/MP-1/playwright/product-overview.spec.ts
+++ testcases/MP-1/playwright/product-overview.spec.ts
@@ -1,25 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Product Overview', () => {
-  test('TC-001 - Verify grid of product cards is displayed on home page', async ({ page }) => {
-    await page.goto('https://testsmith-io.github.io/practice-software-testing/#/');
-    const productCards = await page.locator('div.product-card'); // Assuming product cards are in a div with class 'product-card'
-    await expect(productCards).toHaveCountGreaterThan(0);
-  });
+test('TC-001 - Verify grid of product cards is displayed on home page', async ({ page }) => {
+  await page.goto('https://testsmith-io.github.io/practice-software-testing/#/');
+  const productGrid = await page.getByRole('grid'); // Assuming the grid has a role of 'grid'
+  expect(productGrid).toBeVisible();
+});
 
-  test('TC-002 - Verify product card details are displayed', async ({ page }) => {
-    await page.goto('https://testsmith-io.github.io/practice-software-testing/#/');
-    const productCards = await page.locator('div.product-card');
-    const firstProductCard = productCards.first();
-    await expect(firstProductCard.locator('img')).toBeVisible(); // Assuming images are inside img tags
-    await expect(firstProductCard.locator('h2')).toBeVisible(); // Assuming product name is in an h2 tag
-    await expect(firstProductCard.locator('p.price')).toBeVisible(); // Assuming price is in a p tag with class 'price'
-  });
+test('TC-002 - Verify product card details are displayed', async ({ page }) => {
+  await page.goto('https://testsmith-io.github.io/practice-software-testing/#/');
+  const productCards = await page.locator('.product-card'); // Replace with an appropriate selector for product cards
+  const count = await productCards.count();
+  expect(count).toBeGreaterThan(0);
 
-  test('TC-003 - Verify navigation to product detail page from product card', async ({ page }) => {
-    await page.goto('https://testsmith-io.github.io/practice-software-testing/#/');
-    const firstProductCard = await page.locator('div.product-card').first();
-    await firstProductCard.click();
-    await expect(page).toHaveURL(/.*product-detail/); // Assuming product detail pages contain 'product-detail' in the URL
-  });
+  for (let i = 0; i < count; i++) {
+    const image = await productCards.nth(i).locator('img'); // Assuming each card has an <img> for the product image
+    const name = await productCards.nth(i).locator('.product-name'); // Assuming the product name has a class 'product-name'
+    const price = await productCards.nth(i).locator('.product-price'); // Assuming the product price has a class 'product-price'
+    expect(image).toBeVisible();
+    expect(name).toBeVisible();
+    expect(price).toBeVisible();
+  }
+});
+
+test('TC-003 - Verify navigation to product detail page from product card', async ({ page }) => {
+  await page.goto('https://testsmith-io.github.io/practice-software-testing/#/');
+  const firstProductCard = await page.locator('.product-card').first(); // Assuming the first product card can be selected this way
+  await firstProductCard.click();
+  expect(await page.url()).toMatch(/product-details/); // Adjust according to the actual URL structure for product details
 });
