c36361717c9d4b581874adbeaea5b364440f8833
balatoth <balazs.toth@yahoo.com>
2026-07-30 21:24:57 +0200

    Update testcases/MP-1/playwright/product-overview.spec.ts

--- testcases/MP-1/playwright/product-overview.spec.ts
+++ testcases/MP-1/playwright/product-overview.spec.ts
@@ -3,7 +3,13 @@ import { test, expect } from '@playwright/test';
 const baseURL = 'https://v1.practicesoftwaretesting.com';
 
 test('Display of Product Cards on Home Page', async ({ page }) => {
-    await page.goto(baseURL);
-    const productCards = await page.locator('[data-test="product-1"]').count();
-    expect(productCards).toBeGreaterThan(0);
+  await page.goto(baseURL);
+  const productGrid = await page.locator('[data-test="product-1"]').count();
+  expect(productGrid).toBeGreaterThan(0);
+});
+
+test('Navigation to Product Detail Page', async ({ page }) => {
+  await page.goto(baseURL);
+  await page.click('[data-test="product-1"]');
+  await expect(page).toHaveURL(/\/product\/1/);
 });
