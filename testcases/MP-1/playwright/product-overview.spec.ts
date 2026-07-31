bcf519dfcb99bcbd37019619c742c682670ae983
balatoth <balazs.toth@yahoo.com>
2026-07-31 12:25:34 +0200

    Update testcases/MP-1/playwright/product-overview.spec.ts

--- testcases/MP-1/playwright/product-overview.spec.ts
+++ testcases/MP-1/playwright/product-overview.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('TC-001: Display of Product Cards on Home Page', async ({ page }) => {
-  await page.goto('https://v1.practicesoftwaretesting.com');
-  await expect(page.locator('[data-test="nav-home"]')).toBeVisible();
-  // Verify grid of product cards if a selector is verified in future.
+const BASE_URL = 'https://v1.practicesoftwaretesting.com';
+
+test('TC-001 - Display of Product Cards on Home Page', async ({ page }) => {
+    await page.goto(BASE_URL);
+    await expect(page.locator('[data-test="product-1"]')).toBeVisible();
+    await expect(page.locator('[data-test="product-2"]')).toBeVisible();
+    await expect(page.locator('[data-test="product-3"]')).toBeVisible();
 });
