3af5be9de254108e14ca3143e07cd583683a35cd
balatoth <balazs.toth@yahoo.com>
2026-07-31 11:48:18 +0200

    Update testcases/MP-1/playwright/product-overview.spec.ts

--- testcases/MP-1/playwright/product-overview.spec.ts
+++ testcases/MP-1/playwright/product-overview.spec.ts
@@ -1,10 +1,7 @@
-// This test case verifies the display of product cards on the home page.
 import { test, expect } from '@playwright/test';
 
-const BASE_URL = 'https://v1.practicesoftwaretesting.com';
-
 test('TC-001: Display of Product Cards on Home Page', async ({ page }) => {
-  await page.goto(BASE_URL);
-  // Verify that product card grid is visible
+  await page.goto('https://v1.practicesoftwaretesting.com');
   await expect(page.locator('[data-test="nav-home"]')).toBeVisible();
+  // Verify grid of product cards if a selector is verified in future.
 });
