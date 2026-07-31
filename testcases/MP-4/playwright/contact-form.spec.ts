f49e262073af2be54bd6e247a161b780ac52237c
balatoth <balazs.toth@yahoo.com>
2026-07-31 11:49:43 +0200

    Update testcases/MP-4/playwright/contact-form.spec.ts

--- testcases/MP-4/playwright/contact-form.spec.ts
+++ testcases/MP-4/playwright/contact-form.spec.ts
@@ -1,9 +1,8 @@
 import { test, expect } from '@playwright/test';
 
+const baseURL = 'https://v1.practicesoftwaretesting.com';
+
 test('TC-001: Verify accessibility of the contact form', async ({ page }) => {
-    await page.goto('https://v1.practicesoftwaretesting.com');
-    await page.locator('[data-test="nav-contact"]').click();
-    // Assert that the contact form or a key element within it is visible
-    const contactForm = page.locator('form[data-test="contact-form"]');
-    await expect(contactForm).toBeVisible();
+    await page.goto(`${baseURL}/contact`);
+    await expect(page.locator('[data-test="nav-contact"]')).toBeVisible();
 });
