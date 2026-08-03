2d0441dc76b040c60b7038dc0f41b1180949e40b
balatoth <balazs.toth@yahoo.com>
2026-08-03 18:35:06 +0200

    Update testcases/MP-3/playwright/browse-products-by-category.spec.ts

--- testcases/MP-3/playwright/browse-products-by-category.spec.ts
+++ testcases/MP-3/playwright/browse-products-by-category.spec.ts
@@ -1,10 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-const baseURL = 'https://v1.practicesoftwaretesting.com';
+const BASE_URL = 'https://v1.practicesoftwaretesting.com';
 
-test('TC-001: Display Category Page on Category Click', async ({ page }) => {
-  await page.goto(baseURL);
-  await page.locator('[data-test="nav-hand-tools"]').click();
-  await expect(page).toHaveURL(/hand-tools/);
-  // Add your assertion to check if the category page is displayed
+test('Display Category Page on Category Click', async ({ page }) => {
+    await page.goto(BASE_URL);
+    await page.locator('[data-test="nav-hand-tools"]').click();
+    await expect(page).toHaveURL(/hand-tools/);
 });
