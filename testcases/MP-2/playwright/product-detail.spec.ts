598051f9dfa64dcdd9666da2aac60190f0bc9b50
balatoth <balazs.toth@yahoo.com>
2026-08-04 15:07:26 +0200

    Update testcases/MP-2/playwright/product-detail.spec.ts

--- testcases/MP-2/playwright/product-detail.spec.ts
+++ testcases/MP-2/playwright/product-detail.spec.ts
@@ -2,9 +2,8 @@ import { test, expect } from '@playwright/test';
 
 const BASE_URL = 'https://v1.practicesoftwaretesting.com';
 
-test("Verify Product Detail Page is Displayed", async ({ page }) => {
-    await page.goto(BASE_URL);
-    await page.click('[data-test="product-1"]');
-    await expect(page).toHaveURL(/product/);
-    await expect(page.locator('[data-test="product-name"]')).toBeVisible();
+test('TC-001 - Verify Product Detail Page is Displayed', async ({ page }) => {
+  await page.goto(BASE_URL);
+  await page.click('[data-test="product-1"]');
+  await expect(page).toHaveURL(/product/);
 });
