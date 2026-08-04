ed8df1fc9ef9997cbdeffe6a4317e2f448e4837a
balatoth <balazs.toth@yahoo.com>
2026-08-04 15:07:58 +0200

    Update testcases/MP-4/playwright/contact-form.spec.ts

--- testcases/MP-4/playwright/contact-form.spec.ts
+++ testcases/MP-4/playwright/contact-form.spec.ts
@@ -4,6 +4,5 @@ const baseURL = 'https://v1.practicesoftwaretesting.com';
 
 test('TC-001: Verify accessibility of the contact form', async ({ page }) => {
     await page.goto(`${baseURL}/contact`);
-    const contactForm = page.locator('form#contactForm');
-    await expect(contactForm).toBeVisible();
+    await expect(page.locator('[data-test="nav-contact"]')).toBeVisible();
 });
