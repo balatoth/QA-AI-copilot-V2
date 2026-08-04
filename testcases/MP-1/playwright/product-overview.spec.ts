eef000803ab7c47a3db5747bd87c642e6bde2543
balatoth <balazs.toth@yahoo.com>
2026-08-04 10:56:40 +0200

    Update testcases/MP-1/playwright/product-overview.spec.ts

--- testcases/MP-1/playwright/product-overview.spec.ts
+++ testcases/MP-1/playwright/product-overview.spec.ts
@@ -1,9 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-const baseUrl = 'https://v1.practicesoftwaretesting.com';
+const baseURL = 'https://v1.practicesoftwaretesting.com';
 
 test('TC-001: Display of Product Cards on Home Page', async ({ page }) => {
-  await page.goto(baseUrl);
-  const productCard = page.locator('[data-test="product-1"]');
-  await expect(productCard).toBeVisible();
+    await page.goto(baseURL);
+    const product1 = page.locator('[data-test="product-1"]');
+    const product2 = page.locator('[data-test="product-2"]');
+    const product3 = page.locator('[data-test="product-3"]');
+    const product4 = page.locator('[data-test="product-4"]');
+    const product5 = page.locator('[data-test="product-5"]');
+
+    await expect(product1).toBeVisible();
+    await expect(product2).toBeVisible();
+    await expect(product3).toBeVisible();
+    await expect(product4).toBeVisible();
+    await expect(product5).toBeVisible();
 });
