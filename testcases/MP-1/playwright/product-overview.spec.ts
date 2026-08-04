dcb1bd04416f25a9e23f20f6ee56b7d97ee95126
balatoth <balazs.toth@yahoo.com>
2026-08-04 17:58:35 +0200

    Update testcases/MP-1/playwright/product-overview.spec.ts

--- testcases/MP-1/playwright/product-overview.spec.ts
+++ testcases/MP-1/playwright/product-overview.spec.ts
@@ -6,10 +6,5 @@ test('Display of Product Cards on Home Page', async ({ page }) => {
   await page.goto(BASE_URL);
   await expect(page.locator('[data-test="product-1"]')).toBeVisible();
   await expect(page.locator('[data-test="product-2"]')).toBeVisible();
-});
-
-test('Navigation to Product Detail Page', async ({ page }) => {
-  await page.goto(BASE_URL);
-  await page.locator('[data-test="product-1"]').click();
-  // Assertion for the product detail page goes here, needs evidence
+  await expect(page.locator('[data-test="product-3"]')).toBeVisible();
 });
