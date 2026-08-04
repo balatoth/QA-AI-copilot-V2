7178bae437d2ffaff4e79e61968f9ba8a27392ed
balatoth <balazs.toth@yahoo.com>
2026-08-04 13:27:37 +0200

    Update testcases/MP-1/playwright/product-overview.spec.ts

--- testcases/MP-1/playwright/product-overview.spec.ts
+++ testcases/MP-1/playwright/product-overview.spec.ts
@@ -1,9 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('TC-001 - Display of Product Cards on Home Page', async ({ page }) => {
-    await page.goto('https://v1.practicesoftwaretesting.com');
-    const productCards = page.locator('[data-test^="product-"]');
+const baseURL = 'https://v1.practicesoftwaretesting.com';
+
+test('Display of Product Cards on Home Page', async ({ page }) => {
+    await page.goto(baseURL);
+    const productCards = page.locator('[data-test="product-1"], [data-test="product-2"], [data-test="product-3"], [data-test="product-4"], [data-test="product-5"], [data-test="product-6"], [data-test="product-7"], [data-test="product-8"], [data-test="product-9"], [data-test="product-10"]');
     await expect(productCards).toBeVisible();
-    const count = await productCards.count();
-    expect(count).toBeGreaterThan(0);
+});
+
+test('Navigation to Product Detail Page', async ({ page }) => {
+    await page.goto(baseURL);
+    await page.locator('[data-test="product-1"]').click();
+    await expect(page).toHaveURL(/product/);
 });
