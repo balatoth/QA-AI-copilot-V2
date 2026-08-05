8207aa3b44876ae28f1b67cf8481dd38c413429f
balatoth <balazs.toth@yahoo.com>
2026-08-05 15:59:22 +0200

    Update testcases/MP-1/playwright/product-overview.spec.ts

--- testcases/MP-1/playwright/product-overview.spec.ts
+++ testcases/MP-1/playwright/product-overview.spec.ts
@@ -1,9 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-const baseUrl = 'https://v1.practicesoftwaretesting.com';
-
-test('Display of Product Cards on Home Page', async ({ page }) => {
-    await page.goto(baseUrl);
+test('TC-001 - Display of Product Cards on Home Page', async ({ page }) => {
+    await page.goto('https://v1.practicesoftwaretesting.com');
     await expect(page.locator('[data-test="product-1"]')).toBeVisible();
     await expect(page.locator('[data-test="product-2"]')).toBeVisible();
     await expect(page.locator('[data-test="product-3"]')).toBeVisible();
